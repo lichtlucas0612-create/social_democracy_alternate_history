508bb67e11415aa02a6b5b6c41b4329e85975015
lichtlucas0612-create <lichtlucas0612@gmail.com>
2026-06-07 14:37:33 +0200

    Update d3-linegraph.js
    SPD = SPD
    KPD = Die Linke
    DDP = Bündnis 90/Die Grünen
    Zentrumspartei und BVP = CDU / CDU Unionspartei
    DVP = FDP
    DNVP = AfD
    NSDAP = NPD

--- out/html/d3-linegraph.js
+++ out/html/d3-linegraph.js
@@ -19,10 +19,10 @@ d3.linegraph = function(noTicks, noDots, parties, partyColors, partyNames, dataM
         parties = ['spd', 'kpd', 'ddp', 'z', 'dvp', 'dnvp', 'nsdap', 'other'];
     }
     if (!partyColors) {
-        partyColors = {'spd': '#E3000F', 'kpd': '#8B0000', 'ddp': '#DCCA4A', 'z': '#000', 'dvp': '#D5AC27', 'dnvp': '#3f7bc1', 'nsdap': '#954B00', 'other': '#a0a0a0'};
+        partyColors = {'spd': '#E3000F', 'kpd': '#8B0000', 'ddp': '#008939', 'z': '#000', 'dvp': '#FFE600', 'dnvp': '#3f7bc1', 'nsdap': '#954B00', 'other': '#a0a0a0'};
     }
     if (!partyNames) {
-        partyNames = {'spd': 'SPD', 'kpd': 'KPD', 'ddp': 'DDP', 'z': 'Z + BVP', 'dvp': 'DVP', 'dnvp': 'DNVP', 'nsdap': 'NSDAP', 'other': 'Others'};
+        partyNames = {'spd': 'SPD', 'kpd': 'LINKE', 'ddp': 'B90/G', 'z': 'CDU/CSU', 'dvp': 'FDP', 'dnvp': 'AfD', 'nsdap': 'NPD', 'other': 'Others'};
     }
     if (!additionalMonths) {
         additionalMonths = 10;
